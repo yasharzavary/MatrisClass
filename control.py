7b1e1c53e6a0196f19599465e451a91278d2ef22
yasharzavary <zavaryyashar009@gmail.com>
2024-01-05 09:25:06 -0800

    error class added

---
+++ control.py
@@ -0,0 +1,3 @@
+from matris import Matrix
+
+Matrix()
--- matris.py
+++ matris.py
@@ -1,6 +1,22 @@
 """
 """
 
+class Ex(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.__mes = message
+    def __str__(self):
+        return f'Error: {self.__mes}'
 
 class Matrix:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def check(can):
+        """
+
+        """
+        # temp = len(can[0])
+        # for item in can:
+        #     if len(item) != temp:
